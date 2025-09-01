863fab0530f124b2bfb460fe67062f346c154b56
Omar Nabil <onabil1998@gmail.com>
2025-09-01 22:43:53 +0300

    Integrating Burrito to build the tool into a stand-alone binary

--- invite_tool/lib/invite_tool.ex
+++ invite_tool/lib/invite_tool.ex
@@ -1,5 +1,28 @@
 defmodule InviteTool do
+  use Application
   alias InviteTool.Parser
+
+  @impl true
+  def start(_type, _args) do
+    args = Burrito.Util.Args.argv()
+
+    case args do
+      [file_path] ->
+        case File.read(file_path) do
+          {:ok, issue_text} ->
+            run(issue_text)
+            System.halt(0)
+
+          {:error, reason} ->
+            IO.puts("Failed to read file")
+            System.halt(1)
+        end
+
+      _ ->
+        IO.puts("Usage: invite_tool <file_path>")
+        System.halt(1)
+    end
+  end
 
   def run(issue_text) do
     case Parser.parse_issue_text(issue_text) do
--- invite_tool/mix.exs
+++ invite_tool/mix.exs
@@ -7,21 +7,37 @@ defmodule InviteTool.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: releases()
     ]
   end
+
+  defp releases do
+  [
+    invite_tool: [
+      steps: [:assemble, &Burrito.wrap/1],
+      burrito: [
+        targets: [
+          linux:   [os: :linux,  cpu: :x86_64]
+        ]
+      ]
+    ]
+  ]
+end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {InviteTool, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:instructor, "~> 0.1.0"}
+      {:instructor, "~> 0.1.0"},
+      {:burrito, "~> 1.3.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
